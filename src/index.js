f8ad4295d28cc6b07b7a3a8f94dd54da4cffe91e
Elena Stagg <elenagracestagg@hotmail.co.uk>
2020-09-13 22:47:23 +0100

    Refactored and Lighthouse tested - removed unecessary css, adjusted buttons and added primary container to refactor css - Refactored form validation - Split desktop and modal css into separate css files to reduce render blocking code

--- src/index.js
+++ src/index.js
@@ -9,45 +9,45 @@ const email = document.getElementById("email-input");
 const submitButton = document.getElementById("submit");
 const successMessage = document.getElementById("success-message");
 
-const formElements = [firstName, lastName, email];
+const formInputs = [firstName, lastName, email];
 
 // Open Modal
-downloadButton.addEventListener("click", openModal);
 function openModal() {
   modalContainer.style.display = "flex";
 }
+downloadButton.addEventListener("click", openModal);
 
 // Close Modal
-window.addEventListener("click", outsideClick);
 function outsideClick(event) {
   if (event.target == modalContainer) {
     modalContainer.style.display = "none";
   }
 }
-modalCloseButton.addEventListener("click", closeModal);
+window.addEventListener("click", outsideClick);
 function closeModal() {
   modalContainer.style.display = "none";
 }
+modalCloseButton.addEventListener("click", closeModal);
 
 // Submit form
+function checkInputs() {
+  const isEmpty = (element) => element.value === "";
+  formInputs.forEach((element) => {
+    if (isEmpty(element)) {
+      element.classList.add("error");
+      element.placeholder = "This field should not be empty";
+    } else {
+      element.classList.remove("error");
+    }
+  });
+  if (!formInputs.some(isEmpty)) {
+    submitButton.classList.add("sent");
+    successMessage.style.display = "inline-block";
+    submitButton.innerText = "Sent";
+  }
+}
+
 modalForm.addEventListener("submit", (event) => {
   event.preventDefault();
   checkInputs();
 });
-function checkInputs() {
-  formElements.forEach((element) => {
-    const elementBlank = element.value === "";
-    if (elementBlank) {
-      element.classList.add("error");
-      element.placeholder = "Error";
-    }
-    const elementExists = (element) => element.value !== "";
-    const allElementsExist = formElements.every(elementExists);
-    if (allElementsExist) {
-      submitButton.classList.add("sent");
-      successMessage.style.display = "inline-block";
-      submitButton.innerText = "Sent";
-      element.style.border = "1px solid #d9dfe8";
-    }
-  });
-}
